b34229a9ec1feee2f135d395d329eaa8a9c18fd5
alekey26 <73557654+alekey26@users.noreply.github.com>
2021-07-28 17:55:09 -0500

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,67 @@
+window.sr = ScrollReveal();
+
+	sr.reveal('.spot', {
+		duration: 3000,
+		origin: 'top',
+		distance: '-100px',
+	});
+
+	sr.reveal('.box6', {
+		duration: 3000,
+		origin: 'right',
+		distance: '-250px'
+	});
+
+	sr.reveal('.texto1', {
+		duration: 3000,
+		origin: 'right',
+		distance: '-250px'
+	});
+
+	sr.reveal('.sup2', {
+		duration: 3000,
+		origin: 'left',
+		distance: '-250px'
+	});
+
+	sr.reveal('.integrantes', {
+		duration: 3000,
+		origin: 'top',
+		distance: '-300px'
+	});
+
+	sr.reveal('.sup3', {
+		duration: 3000,
+		origin: 'left',
+		distance: '-250px'
+	});
+	sr.reveal('.video2', {
+		duration: 3000,
+		origin: 'left',
+		distance: '-250px'
+	});
+	sr.reveal('.video3', {
+		duration: 3000,
+		origin: 'right',
+		distance: '-250px'
+	});
+	sr.reveal('.inferior', {
+		duration: 3000,
+		origin: 'left',
+		distance: '-250px'
+	});
+	sr.reveal('.perfil', {
+		duration: 3000,
+		origin: 'right',
+		distance: '350px'
+	});
+	sr.reveal('.frase', {
+		duration: 3000,
+		origin: 'bottom',
+		distance: '300px'
+	});
+	sr.reveal('.red', {
+		duration: 3000,
+		origin: 'left',
+		distance: '300px'
+	});
